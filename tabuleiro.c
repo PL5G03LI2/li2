f13f64e8a6364826ee1acb1dd93f68a18ecd51f2
Miguel Santos <ms8042939@gmail.com>
2025-04-01 14:54:04 +0100

    get_pos()

--- tabuleiro.c
+++ tabuleiro.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+char get_pos(char *tabuleiro, int x, int y)
+{
+    return tabuleiro[y * 5 + x];
+}
+
 void print_tabuleiro(char *tabuleiro, int height, int width)
 {
     for (int i = 0; i < height; i++)
--- tabuleiro.h
+++ tabuleiro.h
@@ -2,3 +2,8 @@
  Imprime o tabuleiro para o ecrã
 */
 void print_tabuleiro(char *tabuleiro, int height, int width);
+
+/*
+ Retorna o char na coordenada (x, y)
+*/
+char get_pos(char *tabuleiro, int x, int y);
