2dc307accf6dbf67240b4c5d95d82542bd700862
Miguel Santos <ms8042939@gmail.com>
2025-04-01 15:15:00 +0100

    fix includes

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <tabuleiro.h>
+#include "tabuleiro.h"
 
 int main()
 {
--- tabuleiro.c
+++ tabuleiro.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <strings.h>
+#include "strings.h"
 
 char get_pos(char *tabuleiro, int x, int y, int height, int width)
 {
