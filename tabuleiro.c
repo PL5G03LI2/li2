072adc25c5c4c65c52220137bfa2943118e6083b
Miguel Santos <ms8042939@gmail.com>
2025-04-01 15:15:43 +0100

    fix some behaviours

--- tabuleiro.c
+++ tabuleiro.c
@@ -23,7 +23,8 @@ void print_tabuleiro(char *tabuleiro, int height, int width)
     {
         for (int j = 0; j < width; j++)
         {
-            print(tabuleiro[i * 5 + j]);
+            printf("%c", tabuleiro[i * 5 + j]);
         }
+        printf("\n");
     }
 }
