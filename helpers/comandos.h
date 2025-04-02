150b007f02862c49419795a5986b01069b39d067
Miguel Santos <ms8042939@gmail.com>
2025-04-02 14:41:11 +0100

    fix: duplicates

--- helpers/comandos.h
+++ helpers/comandos.h
@@ -4,10 +4,11 @@
 
 // Definição da estrutura Tabuleiro que representa um jogo
 // Esta estrutura contém os dados essenciais para manipular o tabuleiro
-typedef struct Tab {
-    char *data;     // Array unidimensional que armazena as células do tabuleiro em formato linha-coluna
-    int height;     // Número de linhas do tabuleiro
-    int width;      // Número de colunas do tabuleiro
+typedef struct
+{
+    char *data; // Array unidimensional que armazena as células do tabuleiro em formato linha-coluna
+    int height; // Número de linhas do tabuleiro
+    int width;  // Número de colunas do tabuleiro
 } Tabuleiro;
 
 // Verifica se uma posição está fora dos limites do tabuleiro
@@ -28,19 +29,19 @@ void set_branco(Tabuleiro *tab, int x, int y);
 
 // Imprime o tabuleiro para o stdout, formatado com espaços entre caracteres
 // Apresenta a visualização completa do estado atual do jogo
-void print_tabuleiro(Tabuleiro *tab, int width, int height);
+void print_tabuleiro(Tabuleiro *tab);
 
 // Carrega um tabuleiro a partir de um ficheiro especificado
 // Retorna 1 se o carregamento for bem-sucedido, 0 caso contrário
-int carregar_tabuleiro(Tabuleiro **tab, const char *filename);
+int carregar_tabuleiro(Tab **tab, const char *filename);
 
 // Guarda o estado atual do tabuleiro num ficheiro
 // Retorna 1 se a gravação for bem-sucedida, 0 caso contrário
-int salvar_tabuleiro(Tabuleiro *tab, const char *filename);
+int salvar_tabuleiro(Tab *tab, const char *filename);
 
 // Verifica se o tabuleiro é válido segundo as regras do jogo
 // Confirma que não há repetições de caracteres nas linhas e colunas
 // Retorna 1 se o tabuleiro for válido, 0 caso contrário
-int validar_tabuleiro(Tabuleiro *tab);
+int validar_tabuleiro(Tab *tab);
 
 #endif
