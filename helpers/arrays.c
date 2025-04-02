4e983ed7581e2eb29cb8b66649b4ac32fb558d0a
Miguel Santos <ms8042939@gmail.com>
2025-04-02 15:25:31 +0100

    pop(in)2 (yes, that's a reference

--- helpers/arrays.c
+++ helpers/arrays.c
@@ -3,11 +3,11 @@
 
 typedef struct node
 {
-    Tab *tab;
+    Tab tab;
     struct node *next;
 } TabHistory;
 
-TabHistory *push(TabHistory *previous, Tab *tab)
+TabHistory *push(TabHistory *previous, Tab tab)
 {
     TabHistory *new = (TabHistory *)malloc(sizeof(TabHistory));
 
@@ -17,4 +17,42 @@ TabHistory *push(TabHistory *previous, Tab *tab)
     new->next = NULL;
 
     return previous;
+}
+
+Tab pop(TabHistory **head)
+{
+    Tab poppedTab;
+
+    // a lista tá vazia
+    if (*head == NULL)
+    {
+        poppedTab.data = NULL;
+        return poppedTab;
+    }
+
+    // a lista só tem um elemento
+    if ((*head)->next == NULL)
+    {
+        poppedTab = (*head)->tab;
+        free(*head);
+        *head = NULL;
+        return poppedTab;
+    }
+
+    // percorre a lista até ao penúltimo elemto
+    TabHistory *current = *head;
+    TabHistory *previous = NULL;
+    while (current->next != NULL)
+    {
+        previous = current;
+        current = current->next;
+    }
+
+    // 'current' a este ponto deve ser o último elemento,
+    // livra a memória e retorna.
+    poppedTab = current->tab;
+    free(current);
+    previous->next = NULL;
+
+    return poppedTab;
 }
