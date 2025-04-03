f3c5631bd07be5bb56c5cd47217bcc9d8f15f38c
Miguel Santos <ms8042939@gmail.com>
2025-04-03 11:38:57 +0100

    new functions yay

--- helpers/strs.c
+++ helpers/strs.c
@@ -1,9 +1,27 @@
 #include "strs.h"
 
+int isLower(char c)
+{
+    return ('a' <= c && c <= 'z');
+}
+
+int isUpper(char c)
+{
+    return ('A' <= c && c <= 'Z');
+}
+
 char toUpper(char c)
 {
-    if (c >= 'a' && c <= 'z')
+    if (isLower(c))
         return c - 32;
+
+    return c;
+}
+
+char toLower(char c)
+{
+    if (isUpper(c))
+        return c + 32;
 
     return c;
 }
