7b9e4eafedd1972eff0f15db63441835e5015580
Miguel Santos <ms8042939@gmail.com>
2025-04-03 11:08:28 +0100

    abbreviate tabuleiro in source

--- jogo/tabuleiro.c
+++ jogo/tabuleiro.c
@@ -2,53 +2,53 @@
 #include "strings.h"
 #include "tabuleiro.h"
 
-int calc_index(Tab *tabuleiro, int x, int y)
+int calc_index(Tab *tab, int x, int y)
 {
-    if (assert_pos(tabuleiro, x, y))
-        return y * tabuleiro->height + x;
+    if (assert_pos(tab, x, y))
+        return y * tab->height + x;
 
     return -1;
 }
 
-int assert_index(Tab *tabuleiro, int i)
+int assert_index(Tab *tab, int i)
 {
-    int max_i = tabuleiro->height * tabuleiro->width;
+    int max_i = tab->height * tab->width;
     return (0 <= i && i <= max_i);
 }
 
-int assert_pos(Tab *tabuleiro, int x, int y)
+int assert_pos(Tab *tab, int x, int y)
 {
-    if (x < 0 || x > tabuleiro->height || y < 0 || y > tabuleiro->width)
+    if (x < 0 || x > tab->height || y < 0 || y > tab->width)
         return 0;
     return 1;
 }
 
-char get_elem(Tab *tabuleiro, int x, int y)
+char get_elem(Tab *tab, int x, int y)
 {
-    if (!assert_pos(tabuleiro, x, y))
+    if (!assert_pos(tab, x, y))
         return '\0';
-    return tabuleiro->data[calc_index(tabuleiro, x, y)];
+    return tab->data[calc_index(tab, x, y)];
 }
 
-void set_elem(Tab *tabuleiro, int x, int y, char c)
+void set_elem(Tab *tab, int x, int y, char c)
 {
-    if (assert_pos(tabuleiro, x, y))
-        tabuleiro->data[calc_index(tabuleiro, x, y)] = c;
+    if (assert_pos(tab, x, y))
+        tab->data[calc_index(tab, x, y)] = c;
 }
 
-void toggle_branco(Tab *tabuleiro, int x, int y)
+void toggle_branco(Tab *tab, int x, int y)
 {
-    if (assert_pos(tabuleiro, x, y))
-        tabuleiro->data[calc_index(tabuleiro, x, y)] = toUpper(get_elem(tabuleiro, x, y));
+    if (assert_pos(tab, x, y))
+        tab->data[calc_index(tab, x, y)] = toUpper(get_elem(tab, x, y));
 }
 
-void print_tabuleiro(Tab *tabuleiro, int height, int width)
+void print_tab(Tab *tab, int height, int width)
 {
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
-            printf("%c", tabuleiro->data[i * 5 + j]);
+            printf("%c", tab->data[i * 5 + j]);
         }
         printf("\n");
     }
