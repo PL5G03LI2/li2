db3a500cdb32f36a68b68dab27f67d011a10da78
Miguel Santos <ms8042939@gmail.com>
2025-04-07 10:14:39 +0100

    update some docs

--- helpers/comandos.h
+++ helpers/comandos.h
@@ -15,6 +15,7 @@ int await_command(char *command);
  @param **args The target tokens array.
 
  @returns The token count.
+ @returns 0 if anything fails to tokenize.
 */
 int tokenize_cmd(char *command, char **args);
 
@@ -25,23 +26,23 @@ int tokenize_cmd(char *command, char **args);
  @param *command Pointer to the string command to parse.
  @param *parsed_cmd Pointer to the target parsed command.
 
- @return CMD_INVALID if unable to parse.
- @return A valid command code if otherwise.
-
- @note Returning CMD_INVALID means returning 0 in case of failure. This is not standard error signal behaviour.
+ @return 1 if unable to parse.
+ @return 0 otherwise.
 */
 int parse_command(char *command, ParsedCommand *parsed_cmd);
 
-// Executa um comando analisado
-// O comando é executado com base no tipo e argumentos fornecidos
-// Retorna 1 se o comando for executado com sucesso, 0 caso contrário
-// O tabuleiro é passado como referência para permitir modificações
-// O comando pode ser um comando de salvar, carregar, selecionar, marcar, verificar, etc.
-// O comando deve ser um ParsedCommand com o tipo e os argumentos
+/*
+ Executa um comando analisado.
+
+ @param *cmd Um comando válido.
+ @param **tab Referência do tabuleiro. Permite ajustes pelo comando.
+
+ @returns 0 se o comando for executado com sucesso, 1 caso contrário.
+*/
 int run_command(ParsedCommand *cmd, Tab **tab);
 
 // Carrega um tabuleiro a partir de um ficheiro especificado
-// Retorna 1 se o carregamento for bem-sucedido, 0 caso contrário
+// @returns 0 se o carregamento for bem-sucedido, 1 caso contrário
 int carregar_tabuleiro(Tab **tab, const char *filename);
 
 // Guarda o estado atual do tabuleiro num ficheiro
