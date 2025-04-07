9828b3eae920a1dfb34ec31edb179367b2edd582
Miguel Santos <ms8042939@gmail.com>
2025-04-07 02:33:39 +0100

    mini teste main

--- main.c
+++ main.c
@@ -7,5 +7,10 @@
 
 int main()
 {
+    char **args = calloc(3, sizeof(char *));
+    for (int i = 0; i < 3; i++)
+        args[i] = calloc(32, sizeof(char));
+
+    printf("%d", tokenize_cmd("g b 4", args));
     return 0;
 }
