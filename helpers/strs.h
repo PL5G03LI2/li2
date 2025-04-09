e0260601fe8db63d067b0d7902e32aee1690e582
Miguel Santos <ms8042939@gmail.com>
2025-04-09 11:46:42 +0100

    add trim_str()

--- helpers/strs.h
+++ helpers/strs.h
@@ -23,3 +23,15 @@ char toUpper(char c);
  Converte um character para minúsculo.
 */
 char toLower(char c);
+
+/**
+ * @brief Trims leading and trailing whitespace from a string.
+ *
+ * @param str The string to be trimmed.
+ *            It should be a null-terminated character array.
+ *
+ * @returns A new pointer to the trimmed string.
+ *
+ * @note If the input string is NULL or an empty string, the function returns without error.
+ */
+char *trim_str(char *str);
